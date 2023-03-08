210e3f129df6ca2b3aa637018693513f21f6535d
deuf <79807627@qq.com>
2023-03-08 20:58:48 +0800

    20230308 stage01 OK

---
+++ stage01/code/day08/demo06 function_parameter.py
@@ -0,0 +1,71 @@
+"""
+    函数参数
+        形式参数parameter
+"""
+
+
+# 缺省（默认）参数：如果实参不提供，可以使用默认值
+def fun01(a=0, b=2, c=0, d=0):
+    print(a)
+    print(b)
+    print(c)
+    print(d)
+
+
+# 关键字实参+缺省形参：调用者可以随意传递参数。
+fun01(b=2, c=3)
+
+
+# 2.位置形参
+
+def fun02(a, b, c, d):
+    print(a)
+    print(b)
+    print(c)
+    print(d)
+
+
+print(1, 2, 3, 4)
+
+
+# 3.星号元组形参：*将所有实参合并为一个元组
+# 作用：让实参个数无限
+def fun03(*args):
+    print(args)
+
+
+fun03()  # ()
+fun03(1)  # (1,)
+fun03(1, "2")  # (1,"2")
+
+
+# 4.关键字形参：在星号元组形参以后的位置形参
+# 目的：要求实参必须使用关键字实参。
+def fun04(a, *args, b):
+    print(a)
+    print(args)
+    print(b)
+
+
+fun04(1, b=2)
+fun04(1, 2, 3, 4, b=2)
+
+
+def fun05(*, a, b):
+    print(a)
+    print(b)
+
+
+fun05(a=1, b=2)
+
+
+# 5.双星号字典形参：实参可以传递数量无线的关键字实参。
+def fun06(**kwargs):
+    print(kwargs)
+
+
+fun06(a=1, b=2)
+
+# 作用：调用fun07
+def fun07(a,b,*args,c,d,**kwargs):
+    pass
